4c869cf3cb1172c58b704b7b4e7aa15ba5d742ad
Ael Gain <ael.gain@gmail.com>
2021-07-19 10:03:28 +0200

    lots of diffs

--- src/Menu.tsx
+++ src/Menu.tsx
@@ -10,12 +10,14 @@ import DeleteIcon from '@material-ui/icons/Delete';
 import db from './config/firebase';
 import {People} from './types';
 
+function createDefaultPeople() :People {
+    return {id: "", name: "", picture: "", bornDate: 1800, deathDate: 1900}
+}
+
 function Menu() {
-    const[selectedPeople, setSelectedPeople] = useState<People>({id: "", name: "", picture: "", bornDate: 0, deathDate: 0});
+    const[selectedPeople, setSelectedPeople] = useState<People>(createDefaultPeople());
     let selectedPeopleModified = {...selectedPeople}
     
-    console.log("Do I reloaded?", selectedPeople)
-
     const[people, setPeople] = useState<Array<People>>([]);
 
     useEffect(() => {
@@ -23,8 +25,8 @@ function Menu() {
     }, []);
 
     const getPeople = async () => {
-        const col = await db.collection('people').get();
-        const people = col.docs.map(p => {return {id: p.id, ...p.data()} as People}).sort((a, b) => (a.name > b.name) ? 1 : -1)
+        const col = await db.collection('people').orderBy('name').get();
+        const people = col.docs.map(p => {return {id: p.id, ...p.data()} as People})
         setPeople(people);
     }
 
@@ -35,8 +37,12 @@ function Menu() {
     const onSubmitPeople = async () => {
         const p = selectedPeopleModified
         if(p.name && p.bornDate && p.deathDate) {
-            await db.collection('people').add(p);
-            await getPeople()
+            const existing = await db.collection('people').where('name', '==', p.name).get();
+            if(!existing.docs.length) {
+                console.log("Creatingg")
+                await db.collection('people').add(p);
+                await getPeople()
+            }
         } 
     }
 
@@ -47,14 +53,14 @@ function Menu() {
     }
       
     return (
-    <List style={{width: '250px'}}>
+    <List style={{display: "flex", flexDirection: "column", height: '97vh'}}>
         <div style={{display: "flex", flexDirection: "column"}}>
-            <TextField id="name" onChange={(e:any) => {selectedPeopleModified.name=e.target.value; setSelectedPeople(selectedPeopleModified)}} helperText="Name" value={selectedPeople.name}/>
-            <TextField id="bornDate" onChange={(e:any) => {selectedPeopleModified.bornDate=e.target.value; setSelectedPeople(selectedPeopleModified)}} helperText="Born date" type="number" value={selectedPeople.bornDate}/>
-            <TextField id="deathDate" onChange={(e:any) => {selectedPeopleModified.deathDate=e.target.value; setSelectedPeople(selectedPeopleModified)}} helperText="Death date" type="number" value={selectedPeople.deathDate}/>
+            <TextField id="name" onChange={(e:any) => {selectedPeopleModified.name=e.target.value; setSelectedPeople(selectedPeopleModified)}} label="Name" value={selectedPeople.name}/>
+            <TextField id="bornDate" onChange={(e:any) => {selectedPeopleModified.bornDate=e.target.value; setSelectedPeople(selectedPeopleModified)}} label="Born date" type="number" value={selectedPeople.bornDate}/>
+            <TextField id="deathDate" onChange={(e:any) => {selectedPeopleModified.deathDate=e.target.value; setSelectedPeople(selectedPeopleModified)}} label="Death date" type="number" value={selectedPeople.deathDate}/>
             <Button variant="contained" color="primary" onClick={onSubmitPeople}>Add</Button>
         </div>
-        <Paper style={{maxHeight: '500px', overflow: 'auto'}}>
+        <Paper style={{overflow: 'auto', maxHeight: '100%'}}>
         {people.map((text) => (
             <div>
             <ListItem button id={text.name} onClick={() => onSelectPeople(text.name)} key={text.name}>
