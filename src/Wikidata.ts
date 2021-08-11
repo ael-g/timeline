a33a4c2ceff33070cd727313a003bcf2809d6932
Ael Gain <ael.gain@gmail.com>
2021-08-11 21:25:17 +0200

    Ok

--- src/Wikidata.ts
+++ src/Wikidata.ts
@@ -63,30 +63,63 @@ const getPeople = async (name: string) : Promise<People[]> => {
     return people
 }
 
-const getOccupation = async (qid: string) => {
+const getQualifiers = async (qid: string, qualifier: string, props: string[]) => {
+    const query = `SELECT DISTINCT ?q_label WHERE {
+        VALUES  ?item               {wd:${qid}}
+
+        ?item   p:${qualifier}      ?q_stmt. 
+        ?q_stmt ps:${qualifier}     ?q.
+        ?q      rdfs:label          ?q_label.
+
+        FILTER(LANG(?q_labels) = "fr" )
+    }`
+
+    const res = await sendQuery(query);
+    return res.map( (i:any) => getField(i, 'q_label') );
+}
+
+const getOccupations = async (qid: string) => {
     const query = `SELECT DISTINCT ?occupation_label WHERE {
         VALUES ?item {wd:${qid}}
-        OPTIONAL { ?item p:P106 ?occupation_stmt. }
-        OPTIONAL { ?occupation_stmt ps:P106 ?occupation.}
-        OPTIONAL { ?occupation rdfs:label ?occupation_label . }
+
+        ?item p:P106 ?occupation_stmt. 
+        ?occupation_stmt ps:P106 ?occupation.
+        ?occupation rdfs:label ?occupation_label. 
 
         FILTER(LANG(?occupation_label) = "fr" )
     }`
 
-    return (await sendQuery(query)).map( (i:any) => ({
-        occupation: capitalize(getField(i, 'occupation_label'))
-    }))
+    return (await sendQuery(query)).map( (i:any) => { return {
+        occupations: capitalize(getField(i, 'occupation_label'))
+    }})
+}
+
+
+const getNotableWork = async (qid: string) => {
+    const query = `SELECT DISTINCT ?position_held_label ?start ?end ?occupation_label ?notablework_label WHERE {
+        VALUES ?item {wd:${qid}}
+
+        ?item p:P800 ?notablework_stmt. 
+        ?notablework_stmt ps:P800 ?notablework.
+        ?notablework rdfs:label ?notablework_label . 
+
+        FILTER(LANG(?notablework_label) = "fr" )
+    }`
+
+    return (await sendQuery(query)).map( (i:any) => { return {
+        notableWork: capitalize(getField(i, "notablework_label")),
+    }})
 }
 
 const getPositionHeld = async (qid: string) => {
-    const query = `SELECT DISTINCT ?position_held_label ?start ?end WHERE {
+    const query = `SELECT DISTINCT ?position_held_label ?start ?end ?occupation_label ?notablework_label WHERE {
         VALUES ?item {wd:${qid}}
 
-        OPTIONAL { ?item p:P39 ?position_held_stmt. }
-        OPTIONAL { ?position_held_stmt ps:P39 ?position_held. }
-        OPTIONAL { ?position_held rdfs:label ?position_held_label. }
-        OPTIONAL { ?position_held_stmt pq:P580 ?start. }
-        OPTIONAL { ?position_held_stmt pq:P582 ?end. }
+        ?item p:P39 ?position_held_stmt. 
+        ?position_held_stmt ps:P39 ?position_held. 
+        ?position_held rdfs:label ?position_held_label. 
+        ?position_held_stmt pq:P580 ?start. 
+        ?position_held_stmt pq:P582 ?end. 
 
         FILTER(LANG(?position_held_label) = "fr" )
     }`
@@ -100,11 +133,14 @@ const getPositionHeld = async (qid: string) => {
 
 // Qualifier
 const getPeopleDetails = async (qid: string) => {
-    const occupations = await getOccupation(qid);
-    const positionHeld = await getPositionHeld(qid);
+    const positionsHeld = await getPositionHeld(qid);
+    const occupations = await getOccupations(qid);
+    const notableWork = await getNotableWork(qid);
+
     return {
+        positionsHeld,
         occupations,
-        positionHeld,
+        notableWork,
     }
 }
 
