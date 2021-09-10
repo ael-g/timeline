e0bb7c855e8b1681f984673563b89531bb8ba751
Ael Gain <ael.gain@gmail.com>
2021-09-10 17:50:18 +0200

    compress in br

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import compress from 'vite-plugin-compress'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,6 +8,7 @@ export default defineConfig({
   // comment this out if that isn't relevant for your project
   build: {
     outDir: 'build',
+    chunkSizeWarningLimit: 2000,
   },
-  plugins: [reactRefresh()],
+  plugins: [reactRefresh(), compress()],
 })
